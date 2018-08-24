225e71a03d2300c857f0501da1e283e109b291b5
arielev <levinsonarie@gmail.com>
2018-08-24 09:44:51 +0300

    Add more unit tests for versions reset

--- helmbump/helmbump_unittest.py
+++ helmbump/helmbump_unittest.py
@@ -23,7 +23,15 @@ class HelmBumpTest(unittest.TestCase):
         self.assertEqual(is_semantic_string('1.2.3'), [1, 2, 3])
         self.assertNotEqual(is_semantic_string('1.2.3'), [1, 2, 4])
         self.assertTrue(is_semantic_string('0.0.0'))
+        self.assertTrue(is_semantic_string('13.0.75'))
+        self.assertTrue(is_semantic_string('0.5.447'))
+        self.assertTrue(is_semantic_string('1.02.3'))
+        self.assertTrue(is_semantic_string('000.000.111'))
         self.assertFalse(is_semantic_string('1.2.c'))
+        self.assertFalse(is_semantic_string('1.2.-3'))
+        self.assertFalse(is_semantic_string('1.2.3-dev'))
+        self.assertFalse(is_semantic_string('1.9'))
+        self.assertFalse(is_semantic_string('text'))
 
     def test_is_valid_helm_chart(self):
         self.assertTrue(is_valid_helm_chart(valid_helm_chart))
@@ -31,9 +39,11 @@ class HelmBumpTest(unittest.TestCase):
         self.assertFalse(is_valid_helm_chart(empty_helm_chart))
 
     def test_bump_version(self):
-        self.assertEqual(bump_version([1, 2, 3], 'major'), [2, 2, 3])
-        self.assertEqual(bump_version([1, 2, 3], 'minor'), [1, 3, 3])
+        self.assertEqual(bump_version([9, 0, 7], 'major'), [10, 0, 0])
+        self.assertEqual(bump_version([1, 2, 3], 'major'), [2, 0, 0])
+        self.assertEqual(bump_version([1, 2, 3], 'minor'), [1, 3, 0])
         self.assertEqual(bump_version([1, 2, 3], 'patch'), [1, 2, 4])
+        self.assertEqual(bump_version([0, 0, 9], 'patch'), [0, 0, 10])
 
 
 if __name__ == '__main__':
