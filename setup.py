6dbee45c24a4a030cd3aeeab45547fa8048599dd
Arie Lev <34907201+ArieLevs@users.noreply.github.com>
2018-09-23 20:25:21 +0300

    Set official release 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="helmbump",
-    version="0.0.13",
+    version="0.1.0",
     author="Arie Lev",
     author_email="levinson.arie@gmail.com",
     description="Helm charts version bumper",
