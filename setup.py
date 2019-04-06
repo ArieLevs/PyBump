03fbb8477655d40ea9adbdf4dd22350241c4acad
jenkins <jenkins@nalkins.cloud>
2019-04-06 08:57:13 +0000

    update version to: 1.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="pybump",
-    version="1.0.5",
+    version="1.0.6",
     author="Arie Lev",
     author_email="levinsonarie@gmail.com",
     description="Python version bumper",
