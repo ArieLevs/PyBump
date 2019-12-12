23661074d2db777a703ce8d7dc4d09e1ef67a1a0
jenkins <jenkins@nalkins.cloud>
2019-12-12 18:41:52 +0000

    update version to: 1.0.14

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="pybump",
-    version="1.0.13",
+    version="1.0.14",
     author="Arie Lev",
     author_email="levinsonarie@gmail.com",
     description="Python version bumper",
