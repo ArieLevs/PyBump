f7429fb351d3044db8c8624da549ec2261d07587
jenkins <jenkins@nalkins.cloud>
2020-02-03 22:18:05 +0000

    update version to: 1.1.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ keywords = ['bump', 'version', 'appVersion', 'versioning', 'helm', 'charts', 'se
 
 setuptools.setup(
     name="pybump",
-    version="1.1.4",
+    version="1.1.5",
     author="Arie Lev",
     author_email="levinsonarie@gmail.com",
     description="Python version bumper",
