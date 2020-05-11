978cc9c7113804513d5123e015f42925d2b2f513
GitHub Action <ArieLevs@users.noreply.github.com>
2020-05-11 21:49:49 +0000

    update version to: 1.2.2 (github action)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ keywords = ['bump', 'version', 'appVersion', 'versioning', 'helm', 'charts', 'se
 
 setuptools.setup(
     name="pybump",
-    version="1.2.1",
+    version="1.2.2",
     author="Arie Lev",
     author_email="levinsonarie@gmail.com",
     description="Python version bumper",
