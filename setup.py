8a6d1cfbffa06acdef59a08b914d6cbc164f9712
Arie Lev <levinsonarie@gmail.com>
2020-08-14 00:48:33 +0300

    add supported python versions

--- setup.py
+++ setup.py
@@ -23,6 +23,8 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
