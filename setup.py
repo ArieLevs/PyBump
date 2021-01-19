b0a8cd3fc874d749f3ac67d151f01f26bcbcd623
GitHub Action <ArieLevs@users.noreply.github.com>
2021-01-19 19:01:47 +0000

    update version to: 1.6.2 (github action)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ keywords = ['bump', 'version', 'appVersion', 'versioning', 'helm', 'charts', 'se
 
 setuptools.setup(
     name="pybump",
-    version="1.6.1",
+    version="1.6.2",
     author="Arie Lev",
     author_email="levinsonarie@gmail.com",
     description="Python version bumper",
