ff0f2b07e98f787e35b3e13dd80fda37681fb8c4
arielev <levinsonarie@gmail.com>
2025-03-29 18:09:40 +0300

    start using pyproject.toml

--- setup.py
+++ setup.py
@@ -22,31 +22,5 @@ with open("README.rst", "r") as fh:
 keywords = ['bump', 'version', 'appVersion', 'versioning', 'helm', 'charts', 'setup.py', 'promote']
 
 setuptools.setup(
-    name="pybump",
-    version="1.12.6",
-    author="Arie Lev",
-    author_email="levinsonarie@gmail.com",
-    description="Python version bumper",
-    keywords=keywords,
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
-    url="https://github.com/ArieLevs/PyBump",
-    license='Apache License 2.0',
-    packages=setuptools.find_packages(),
-    install_requires=parse_requirements("requirements.txt"),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-    ],
-    entry_points={
-        'console_scripts': [
-            'pybump = src.pybump:main'
-        ],
-    },
+    name="pybump"
 )
